ba8e030ddb139cffbaff69a4b31c57b0ddb7389e
Munna-Khandakar <munnashisad@gmail.com>
2024-08-29 22:31:08 +0600

    api changed

--- lib/apiInstance.ts
+++ lib/apiInstance.ts
@@ -5,7 +5,7 @@ interface ApiInstance extends AxiosInstance {
     (config: AxiosRequestConfig): Promise<any>;
 }
 
-const backend_url: string = 'https://eadb-61-247-182-213.ngrok-free.app/pharmatica';
+const backend_url: string = 'https://1893-61-247-182-213.ngrok-free.app/pharmatica';
 
 const api: ApiInstance = axios.create({
     baseURL: `${backend_url}/api`,
