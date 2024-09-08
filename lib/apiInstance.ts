229f9e8956670780ff0793e495f5a3bc6688f333
Munna-Khandakar <munnashisad@gmail.com>
2024-09-08 21:30:16 +0600

    admin can track user

--- lib/apiInstance.ts
+++ lib/apiInstance.ts
@@ -5,7 +5,7 @@ interface ApiInstance extends AxiosInstance {
     (config: AxiosRequestConfig): Promise<any>;
 }
 
-const backend_url: string = 'https://2973-61-247-182-213.ngrok-free.app/pharmatica';
+const backend_url: string = 'https://3489-61-247-182-213.ngrok-free.app/pharmatica';
 
 const api: ApiInstance = axios.create({
     baseURL: `${backend_url}/api`,
