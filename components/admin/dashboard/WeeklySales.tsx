5a1c4cf15b44eed98d636c04c7872f53d6a63831
Munna-Khandakar <munnashisad@gmail.com>
2024-09-15 23:21:11 +0600

    api changes

--- components/admin/dashboard/WeeklySales.tsx
+++ components/admin/dashboard/WeeklySales.tsx
@@ -1,8 +1,19 @@
 import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from '@/components/ui/card';
 import {ChartContainer, ChartTooltip, ChartTooltipContent} from '@/components/ui/chart';
 import {Bar, BarChart, Label, Rectangle, ReferenceLine, XAxis} from 'recharts';
+import api from '@/lib/apiInstance';
+import useSWR from 'swr';
+
+const fetcher = (url: string) => api.get(url).then((res) => res.data);
 
 export const WeeklySales = () => {
+
+    const {
+        data,
+        isLoading,
+        mutate
+    } = useSWR<any>('report/order/weekly-details', fetcher, {revalidateOnFocus: false});
+
     return (
         <Card x-chunk="charts-01-chunk-0">
             <CardHeader className="space-y-0 pb-2">
@@ -14,108 +25,87 @@ export const WeeklySales = () => {
                 </CardTitle>
             </CardHeader>
             <CardContent>
-                <ChartContainer
-                    config={{
-                        steps: {
-                            label: 'Steps',
-                            color: 'hsl(var(--chart-1))',
-                        },
-                    }}
-                >
-                    <BarChart
-                        accessibilityLayer
-                        margin={{
-                            left: -4,
-                            right: -4,
-                        }}
-                        data={[
-                            {
-                                date: '2024-01-01',
-                                steps: 2000,
-                            },
-                            {
-                                date: '2024-01-02',
-                                steps: 2100,
-                            },
-                            {
-                                date: '2024-01-03',
-                                steps: 2200,
-                            },
-                            {
-                                date: '2024-01-04',
-                                steps: 1300,
-                            },
-                            {
-                                date: '2024-01-05',
-                                steps: 1400,
-                            },
-                            {
-                                date: '2024-01-06',
-                                steps: 2500,
-                            },
-                            {
-                                date: '2024-01-07',
-                                steps: 1600,
-                            },
-                        ]}
-                    >
-                        <Bar
-                            dataKey="steps"
-                            fill="var(--color-steps)"
-                            radius={5}
-                            fillOpacity={0.6}
-                            activeBar={<Rectangle fillOpacity={0.8}/>}
-                        />
-                        <XAxis
-                            dataKey="date"
-                            tickLine={false}
-                            axisLine={false}
-                            tickMargin={4}
-                            tickFormatter={(value) => {
-                                return new Date(value).toLocaleDateString('en-US', {
-                                    weekday: 'short',
-                                });
+
+                {
+                    isLoading
+                        ?  <WeeklySalesSkeleton />
+                        :
+                        <ChartContainer
+                            config={{
+                                steps: {
+                                    label: 'Orders',
+                                    color: 'hsl(var(--chart-1))',
+                                },
                             }}
-                        />
-                        <ChartTooltip
-                            defaultIndex={2}
-                            content={
-                                <ChartTooltipContent
-                                    hideIndicator
-                                    labelFormatter={(value) => {
+                        >
+                            <BarChart
+                                accessibilityLayer
+                                margin={{
+                                    left: -4,
+                                    right: -4,
+                                }}
+                                data={[
+                                    ...data
+                                ]}
+                            >
+                                <Bar
+                                    dataKey="count"
+                                    fill="var(--color-steps)"
+                                    radius={5}
+                                    fillOpacity={0.6}
+                                    activeBar={<Rectangle fillOpacity={0.8}/>}
+                                />
+                                <XAxis
+                                    dataKey="day"
+                                    tickLine={false}
+                                    axisLine={false}
+                                    tickMargin={4}
+                                    tickFormatter={(value) => {
                                         return new Date(value).toLocaleDateString('en-US', {
-                                            day: 'numeric',
-                                            month: 'long',
-                                            year: 'numeric',
+                                            weekday: 'short',
                                         });
                                     }}
                                 />
-                            }
-                            cursor={false}
-                        />
-                        <ReferenceLine
-                            y={1200}
-                            stroke="hsl(var(--muted-foreground))"
-                            strokeDasharray="3 3"
-                            strokeWidth={1}
-                        >
-                            <Label
-                                position="insideBottomLeft"
-                                value="Average Steps"
-                                offset={10}
-                                fill="hsl(var(--foreground))"
-                            />
-                            <Label
-                                position="insideTopLeft"
-                                value="12,343"
-                                className="text-lg"
-                                fill="hsl(var(--foreground))"
-                                offset={10}
-                                startOffset={100}
-                            />
-                        </ReferenceLine>
-                    </BarChart>
-                </ChartContainer>
+                                <ChartTooltip
+                                    defaultIndex={2}
+                                    content={
+                                        <ChartTooltipContent
+                                            hideIndicator
+                                            labelFormatter={(value) => {
+                                                return new Date(value).toLocaleDateString('en-US', {
+                                                    day: 'numeric',
+                                                    month: 'long',
+                                                    year: 'numeric',
+                                                });
+                                            }}
+                                        />
+                                    }
+                                    cursor={false}
+                                />
+                                <ReferenceLine
+                                    y={1200}
+                                    stroke="hsl(var(--muted-foreground))"
+                                    strokeDasharray="3 3"
+                                    strokeWidth={1}
+                                >
+                                    <Label
+                                        position="insideBottomLeft"
+                                        value="Average Steps"
+                                        offset={10}
+                                        fill="hsl(var(--foreground))"
+                                    />
+                                    <Label
+                                        position="insideTopLeft"
+                                        value="12,343"
+                                        className="text-lg"
+                                        fill="hsl(var(--foreground))"
+                                        offset={10}
+                                        startOffset={100}
+                                    />
+                                </ReferenceLine>
+                            </BarChart>
+                        </ChartContainer>
+                }
             </CardContent>
             <CardFooter className="flex-col items-start gap-1">
                 <CardDescription>
@@ -131,3 +121,29 @@ export const WeeklySales = () => {
         </Card>
     );
 };
+
+export function WeeklySalesSkeleton() {
+    return (
+        <Card x-chunk="charts-01-chunk-0">
+            <CardHeader className="space-y-0 pb-2">
+                <CardDescription>
+                    <div className="h-4 w-1/2 bg-gray-300 rounded animate-pulse"></div>
+                </CardDescription>
+                <CardTitle className="text-4xl tabular-nums">
+                    <div className="h-8 w-1/4 bg-gray-300 rounded animate-pulse"></div>
+                </CardTitle>
+            </CardHeader>
+            <CardContent>
+                <div className="w-full h-48 bg-gray-300 animate-pulse"></div>
+            </CardContent>
+            <CardFooter className="flex-col items-start gap-1">
+                <CardDescription>
+                    <div className="h-4 w-3/4 bg-gray-300 rounded animate-pulse"></div>
+                </CardDescription>
+                <CardDescription>
+                    <div className="h-4 w-1/2 bg-gray-300 rounded animate-pulse"></div>
+                </CardDescription>
+            </CardFooter>
+        </Card>
+    );
+}
