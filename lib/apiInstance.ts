71f2df4cc41060547fdf140e2b3c5d670e0e4bd3
Munna-Khandakar <munnashisad@gmail.com>
2024-09-28 20:26:41 +0600

    live api end point added

--- lib/apiInstance.ts
+++ lib/apiInstance.ts
@@ -5,7 +5,7 @@ interface ApiInstance extends AxiosInstance {
     (config: AxiosRequestConfig): Promise<any>;
 }
 
-const backend_url: string = 'https://b53b-61-247-182-213.ngrok-free.app/pharmatica';
+const backend_url: string = 'http://139.59.233.61:8080/pharmatica';
 
 const api: ApiInstance = axios.create({
     baseURL: `${backend_url}/api`,
